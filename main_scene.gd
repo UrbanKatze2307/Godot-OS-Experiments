e9113a6eb1435c950b66d44800aad1d39363d0ba
UrbanElisa <cat.skardo@gmail.com>
2023-01-08 14:22:17 +0100

    Added the toggle passthrough button

--- main_scene.gd
+++ main_scene.gd
@@ -7,6 +7,7 @@ var shake_power: int = 2
 var window_original_position: Vector2
 
 var shake_window: bool = false
+var passthrough: bool = false
 
 
 
@@ -38,3 +39,12 @@ func _on_timer_shake_timeout() -> void:
 func _on_button_transparency_pressed() -> void:
 	get_tree().get_root().transparent = !get_tree().get_root().transparent
 	get_tree().get_root().transparent_bg = !get_tree().get_root().transparent_bg
+
+
+func _on_button_passthrough_pressed() -> void:
+	match passthrough:
+		true:
+			DisplayServer.window_set_mouse_passthrough([])
+		false:
+			DisplayServer.window_set_mouse_passthrough([$ButtonPassthrough.position, Vector2($ButtonPassthrough.position.x + $ButtonPassthrough.size.x, $ButtonPassthrough.position.y), $ButtonPassthrough.position + $ButtonPassthrough.size, Vector2($ButtonPassthrough.position.x, $ButtonPassthrough.position.y + $ButtonPassthrough.size.y)])
+	passthrough = !passthrough
